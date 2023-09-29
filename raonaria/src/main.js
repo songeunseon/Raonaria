d1784c81b14ca6df3082f4bb4a7f1c7a73f5f32e
gygid27 <gygid27@naver.com>
2023-09-29 21:45:59 +0900

    2023.09.29

--- raonaria/src/main.js
+++ raonaria/src/main.js
@@ -5,19 +5,14 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import  
 
-// import 'bootstrap/dist/css/bootstrap.min.css'
-// import 'bootstrap/dist/js/bootstrap.esm.min.js'
+// createApp(App).mount('#app')
 
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
 
 
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-
-app.mount('#app')
+app.mount('#app');
